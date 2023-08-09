f17376844c29691a7b99276c1dcbca373d32cc4c
Eddie2111 <tarek42223@gmail.com>
2023-08-09 10:25:48 +0600

    feat(style):sign in page designed

--- src/pages/signin/index.tsx
+++ src/pages/signin/index.tsx
@@ -3,11 +3,15 @@ import React from 'react'
 import { Container } from '@nextui-org/react'
 
 import dynamic from 'next/dynamic'
+import Link from 'next/link'
 import { useRouter } from 'next/router'
 
 import { useSession, signIn } from 'next-auth/react'
 import { FcGoogle } from 'react-icons/fc'
 import { FaFacebookF } from 'react-icons/fa'
+
+import { Button, Text, Row, Col, Card } from '@nextui-org/react'
+
 
 import ManagerLogin from '@/components/forms/ManagerLogin'
 import DriverLogin from '@/components/forms/DriverLogin'
@@ -56,16 +60,60 @@ export default function Signin() {
         console.log('closed')
     }
     return (
-        <div>
-            <Container
-                display="flex"
-                alignItems="center"
-                justify="center"
-                css={{ minHeight: '100vh' }}
-            >
-                <ManagerLogin />
-                <DriverLogin />
-            </Container>
+    <div className="mt-[-10px] md:mt-[0px]">
+        <p className="justify-center text-center text-4xl font-semibold mb-5">
+            Sign in to your account
+        </p>
+        <Container
+            display="flex"
+            justify="center"
+            css={{ minHeight: '50vh' }}
+        >
+            <Card isPressable css={{w:"280px",h:"200px"}} style={{marginRight:"25px"}}>
+                <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                    src="/animations/manager.gif"
+                    objectFit="cover"
+                    width="40%"
+                    height={140}
+                    alt={"Orange"}
+                />
+                </Card.Body>
+                <Card.Footer css={{ justifyItems: "flex-start" }}>
+                <Row wrap="wrap" justify="space-between" align="center">
+                    <Text b>Manager</Text>
+                    <ManagerLogin />
+                </Row>
+                </Card.Footer>
+            </Card>
+
+            <Card isPressable css={{w:"280px",h:"200px"}}>
+                <Card.Body css={{ p: 0 }}>
+                <Card.Image
+                    src="/animations/driver.gif"
+                    objectFit="cover"
+                    width="40%"
+                    height="100%"
+                    alt={"Orange"}
+                />
+                </Card.Body>
+                <Card.Footer css={{ justifyItems: "flex-start" }}>
+                <Row wrap="wrap" justify="space-between" align="center">
+                    <Text b>Driver</Text>
+                    <DriverLogin />
+                </Row>
+                </Card.Footer>
+            </Card>
+
+    </Container>
+
+    <p className="justify-center text-center text-4xl font-semibold mb-5">
+            No Account?  &nbsp;
+            <Link href="/signup" className="text-blue-500 hover:text-blue-600 hover:underline hover:underline-offset-1 duration-300">
+                Sign up today
+            </Link>
+        </p>
+
         </div>
     )
 }
